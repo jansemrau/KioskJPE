41b40d30b323226862899943540e18bb1f578f9b
Jan Semrau <janmalte2000@web.de>
2022-05-19 14:27:52 +0200

    WIP: Avoid popup blocker

--- frontend/utils/Print.js
+++ frontend/utils/Print.js
@@ -1,10 +1,6 @@
 const createPrintableTable = () => {
-    var id = new Date().getTime();
-    let newWin = window.open(
-        window.location.href + "?printerFriendly=true",
-        id +
-            "toolbar=1,scrollbars=1,location=0,statusbar=0,menubar=1,resizable=1,width=800,height=600,left = 240,top = 212"
-    );
+    let newWin = window.open("");
+    newWin.location.href = url;
     newWin.document.write(`<html><body><div id='table'><table
             style="border: 1px solid black; font-family: Arial, Helvetica, sans-serif; font-size: 1rem; border-collapse:collapse;"
             id="printTable"
